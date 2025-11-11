23d88e64ae30bbca6103d5eac03964947bffccae
Uriah Finkel <Ufinkel@gmail.com>
2025-11-11 20:53:01 +0200

    docs: align tutorial with pearl personalized example

--- src/probounds/__init__.py
+++ src/probounds/__init__.py
@@ -1,7 +1,32 @@
 # read version from installed package
 from importlib.metadata import PackageNotFoundError, version
 
+from .probounds import (
+    calculate_bounds_combined,
+    calculate_bounds_combined_by_feature,
+    calculate_bounds_experimental,
+    calculate_bounds_experimental_from_probounds_data,
+    calculate_bounds_observed,
+    calculate_bounds_observed_by_feature,
+    calculate_bounds_observed_from_probounds_data,
+    create_probounds_crosstab,
+    probounds_crosstab_feature,
+)
+
 try:  # pragma: no cover - fall back if package isn't installed
     __version__ = version("probounds")
 except PackageNotFoundError:  # pragma: no cover - allow tests to run without install
     __version__ = "0.0.0"
+
+__all__ = [
+    "__version__",
+    "calculate_bounds_combined",
+    "calculate_bounds_combined_by_feature",
+    "calculate_bounds_experimental",
+    "calculate_bounds_experimental_from_probounds_data",
+    "calculate_bounds_observed",
+    "calculate_bounds_observed_by_feature",
+    "calculate_bounds_observed_from_probounds_data",
+    "create_probounds_crosstab",
+    "probounds_crosstab_feature",
+]
